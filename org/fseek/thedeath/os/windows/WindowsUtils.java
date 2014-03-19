a1bdf3a2af363045f3a928af9d5ba07fc1b9e57c
Simon Wimmesberger <wimmesberger@gmail.com>
2014-03-19 11:25:39 +0100

    Replaced registry reading via command line with a more proper solution
    Props to the stackoverflow user:
    
    http://stackoverflow.com/questions/62289/read-write-to-windows-registry-using-java

--- org/fseek/thedeath/os/windows/WindowsUtils.java
+++ org/fseek/thedeath/os/windows/WindowsUtils.java
@@ -1,84 +1,17 @@
 package org.fseek.thedeath.os.windows;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.StringWriter;
+import java.lang.reflect.InvocationTargetException;
 
 public class WindowsUtils
 {
+    private static final String DESKTOP_FOLDER_CMD = "Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Shell Folders";
 
-    private static final String REGQUERY_UTIL = "reg query ";
-    private static final String REGSTR_TOKEN = "REG_SZ";
-    private static final String DESKTOP_FOLDER_CMD = REGQUERY_UTIL
-    + "\"HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\"
-    + "Explorer\\Shell Folders\" /v ";
-
-    private WindowsUtils(){}
-
-    public static String getCurrentUserPath(String search)
+    public static String getCurrentUserPath(String key)
     {
-        try
-        {
-            Process process = Runtime.getRuntime().exec(DESKTOP_FOLDER_CMD + "\"" + search + "\"");
-            StreamReader reader = new StreamReader(process.getInputStream());
-
-            reader.start();
-            process.waitFor();
-            reader.join();
-
-            String result = reader.getResult();
-            reader.close();
-            int p = result.indexOf(REGSTR_TOKEN);
-
-            if (p == -1)
-            {
-                return null;
-            }
-
-            return result.substring(p + REGSTR_TOKEN.length()).trim();
-        } catch (IOException | InterruptedException e)
-        {
-            return null;
+        try {
+          return WinRegistry.readString(WinRegistry.HKEY_CURRENT_USER, DESKTOP_FOLDER_CMD, key);
+        } catch (IllegalArgumentException | IllegalAccessException | InvocationTargetException e) {
+          return null;
         }
-    }
-
-
-    static class StreamReader extends Thread
-    {
-
-        private InputStream is;
-        private StringWriter sw;
-
-        StreamReader(InputStream is)
-        {
-            this.is = is;
-            sw = new StringWriter();
-        }
-
-        @Override
-        public void run()
-        {
-            try
-            {
-                int c;
-                while ((c = is.read()) != -1)
-                {
-                    sw.write(c);
-                }
-            } catch (IOException e)
-            {
-            }
-        }
-
-        String getResult()
-        {
-            return sw.toString();
-        }
-        
-        public void close() throws IOException{
-            is.close();
-            sw.close();
-        }
-        
     }
 }
