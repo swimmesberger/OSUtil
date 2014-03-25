78b3c64ecaab2626c8e77739b78f96daaad2f709
Simon Wimmesberger <wimmesberger@gmail.com>
2014-03-25 22:06:44 +0100

    Much safer and solid usage of reg.exe

--- org/fseek/thedeath/os/windows/registry/RegHandler.java
+++ org/fseek/thedeath/os/windows/registry/RegHandler.java
@@ -24,23 +24,23 @@
 package org.fseek.thedeath.os.windows.registry;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
 import org.fseek.thedeath.os.util.Debug;
-import org.fseek.thedeath.os.util.UtilBox;
 
 public class RegHandler implements INativeRegistryHandler {
-
+    private static final int TIMEOUT = 500;
     public RegHandler() {
     }
 
     /**
      * ******************************************************************************************************************************
      * Method saves any variable to the registry via the regedit.exe and runtime
-     * - because its java 1.4 compatbile (otherwhise it would be the
-     * processbuilder)
      *
      * @param path String the registy path (without [])
      * @param valueName String the valuename to set
@@ -50,7 +50,7 @@ public class RegHandler implements INativeRegistryHandler {
      * @return boolean returns always true (otherwhise exceptio is thrown) -
      * maybe better use in future
      * @throws RegistryErrorException
-     ******************************************************************************************************************************
+     * *****************************************************************************************************************************
      */
     @Override
     public boolean saveAnyValue(String path, String valueName, String type, String data) throws RegistryErrorException {
@@ -64,20 +64,20 @@ public class RegHandler implements INativeRegistryHandler {
             } else if (type.equals(WinRegistry.EXPAND_KEY_IDENT)) {
                 type = "REG_EXPAND_SZ";
             }
-            Runtime.getRuntime().exec("reg add \"" + path + "\" /v \"" + valueName + "\" /t " + type + " /d \"" + data + "\" /f");
+            //Runtime.getRuntime().exec("reg add \"" + path + "\" /v \"" + valueName + "\" /t " + type + " /d \"" + data + "\" /f");
+            ProcessBuilder pb = new ProcessBuilder("reg", "add", path, "/v", valueName, "/t", type, "/d", data, "/f");
+            Process process = pb.start();
+            return process.waitFor() == 0;
         } catch (Exception ex) {
             //        ex.printStackTrace(System.out);
             Debug.printException(ex);
             throw RegistryErrorException.getException(ex);
         }
-        return true;
     }
 
     /**
      * ********************************************************************************************************************************
-     * Method extracts any variable from the registry via the regedit.exe and
-     * runtime - because its java 1.4 compatbile (otherwhise it would be the
-     * processbuilder)
+     * Method extracts any variable from the registry via the regedit.exe
      *
      * @param path String the registry path to the parent key
      * @param valueName String the valuename which should be read from the
@@ -86,77 +86,63 @@ public class RegHandler implements INativeRegistryHandler {
      * @return String null if the valuename is not found or the path could not
      * be exported - otherwhise the data from the registry
      * @throws RegistryErrorException
-     ********************************************************************************************************************************
+     * *******************************************************************************************************************************
      */
     @Override
     public String extractAnyValue(String path, String valueName, boolean appendType) throws RegistryErrorException {
-        StringBuilder strRet = new StringBuilder(); //stringbuffer for appending, if an entry has multiplie lines
-        BufferedReader br = null;
-        File f = null;
         try {
-            f = File.createTempFile("regorexp", ".jta"); //creates tmp File for storing the registry key
-            //ATTENTION!! THESE COULD BE A DEADLOCK BECAUSE I WAITFOR THE END OF PROCESS HERE
-            Runtime.getRuntime().exec("cmd /c \"reg query \"" + path + "\" /v \"" + valueName + "\" > " + f.getAbsolutePath() + " 2>&1\"").waitFor(); //<-- WAITING FOR END OF PROCESS
-            UtilBox._waitForFile(f); //wait until the file size is not increasing anymore
-            br = new BufferedReader(new InputStreamReader(new FileInputStream(f)));
-            String line = "";
-            boolean lineFound = false;
-            while ((line = br.readLine()) != null) {
-                if (line.equals(path)) {
-                    lineFound = true;
-                } else if (lineFound && line.trim().length() > 0) {
-                    StringTokenizer st = new StringTokenizer(line, " \t");
-                    String[] items = {"", "", ""};
-                    int i = 0;
-                    while (st.hasMoreTokens()) {
-                        items[i] += st.nextToken() + " ";
-                        if (i < 2) {
-                            i++;
+            StringBuilder strRet = new StringBuilder(); //stringbuffer for appending, if an entry has multiplie lines
+            ProcessBuilder pb = new ProcessBuilder("cmd", "/c", "reg", "query", path, "/v", valueName);
+            Process process = pb.start();
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()))){
+                Callable<String> readTask = new Callable<String>() {
+                    @Override
+                    public String call() throws Exception {
+                        return br.readLine();
+                    }
+                };
+                Future<String> future = new FutureTask(readTask);
+                boolean lineFound = false;
+                while (true) {
+                    String line = future.get(TIMEOUT, TimeUnit.MILLISECONDS);
+                    if(line == null)break;
+                    if (line.equals(path)) {
+                        lineFound = true;
+                    } else if (lineFound && line.trim().length() > 0) {
+                        StringTokenizer st = new StringTokenizer(line, " \t");
+                        String[] items = {"", "", ""};
+                        int i = 0;
+                        while (st.hasMoreTokens()) {
+                            items[i] += st.nextToken() + " ";
+                            if (i < 2) {
+                                i++;
+                            }
+                        }
+                        for (int j = 0; j < items.length; j++) {
+                            items[j] = items[j].trim();
+                        }
+                        if (items[0].equals(valueName)) {
+                            if (appendType) {
+                                strRet.append(items[1]);
+                                strRet.append(" ");
+                            }
+                            strRet.append(items[2]);
+                            if (items[1].equals("REG_MULTI_SZ") && strRet.toString().endsWith("\\0\\0")) {
+                                strRet.setLength(strRet.length() - 4);
+                            }
+                            break;
                         }
                     }
-                    for (int j = 0; j < items.length; j++) {
-                        items[j] = items[j].trim();
-                    }
-                    if (items[0].equals(valueName)) {
-                        if (appendType) {
-                            strRet.append(items[1]);
-                            strRet.append(" ");
-                        }
-                        //[0] = type
-                        //[1] = entry
-                        /*              if (items[0].equals("REG_MULTI_SZ"))
-                         strRet.append(MULTI_KEY); //add this for older version
-                         else if (items[0].equals("REG_EXPAND_SZ"))
-                         strRet.append(EXPAND_KEY);
-                         else if (items[0].equals("REG_DWORD"))
-                         strRet.append(DWORD_KEY);
-                         else if (items[0].equals("REG_BINARY"))
-                         strRet.append(BINARY_KEY);*/
-                        strRet.append(items[2]);
-                        if (items[1].equals("REG_MULTI_SZ") && strRet.toString().endsWith("\\0\\0")) {
-                            strRet.setLength(strRet.length() - 4);
-                        }
-                        break;
-                    }
-                }
-            }
-        } catch (Exception ex) {
-            //        ex.printStackTrace(System.out);
-            Debug.printException(ex);
-            throw RegistryErrorException.getException(ex);
-        } finally {
-            try {
-                if (br != null) {
-                    br.close(); //close reader, so that you can delete the file
                 }
             } catch (Exception ex) {
+                //        ex.printStackTrace(System.out);
+                Debug.printException(ex);
+                throw RegistryErrorException.getException(ex);
             }
-            if (f != null) {
-                if (!f.delete()) {
-                    f.deleteOnExit(); //mark it, for delete on exit
-                }
-            }
+            return strRet.toString();
+        } catch (IOException ex) {
+            Debug.printException(ex);
+            throw RegistryErrorException.getException(ex);
         }
-        return strRet.toString();
     }
 }
